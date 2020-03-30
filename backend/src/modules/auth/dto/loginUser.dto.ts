64993b74e44ca98ad40b00ff16a6c9f2aafdcc4b
magdalena-u <magdalena.urzon@gmail.com>
2020-03-30 17:40:40 +0200

    add postgres database

--- backend/src/modules/auth/dto/loginUser.dto.ts
+++ backend/src/modules/auth/dto/loginUser.dto.ts
@@ -11,3 +11,15 @@ export class LoginUserDto {
   })
   password: string;
 }
+
+export class RegisterUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(3, {
+    message: 'Password is too short. It should have at least 3 characters',
+  })
+  password: string;
+}
--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { UsersService } from './users.service';
+import { DatabaseModule } from 'src/database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Account } from './users.entity';
 
 @Module({
+  imports: [DatabaseModule, TypeOrmModule.forFeature([Account])],
   providers: [UsersService],
   exports: [UsersService],
 })
